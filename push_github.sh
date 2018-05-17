6d0f5c227e403c8c65e1b266bfdbe7897cd36230
Haneesh Kesari <hkesari@Haneeshs-MacBook-Pro.local>
2018-05-16 20:34:16 -0400

    script pushing

---
+++ push_github.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git pull origin master
+git add .
+git status
+git commit -m "script pushing"
+git push origin master
+#open  https://appliedmechanicslab.github.io/appliedmechanicslab/
